0a0c76d1c7bdad03756d5dcfab07e9720747f39c
Pa70rr <pa70rr@gmail.com>
2025-10-29 00:59:43 +0200

    Доопрацював сортування в функцію

--- task02.py
+++ task02.py
@@ -2,13 +2,12 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     if min >= 1 and max <1000 and quantity < (max - min + 1):
-        return random.sample(range(min, max), quantity)
+        return sorted(random.sample(range(min, max), quantity))
     else:
         return []
     
 
 lottery_numbers = get_numbers_ticket(1, 36, 5)
-lottery_numbers.sort()
 
 print("Ваші лотерейні числа:", lottery_numbers)
 

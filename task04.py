9a41b613456ad3f160915313583163215df58055
Pa70rr <pa70rr@gmail.com>
2025-10-29 22:47:29 +0200

    Вніс змінні в функцію

--- task04.py
+++ task04.py
@@ -5,10 +5,9 @@ users = [
     {"name": "Jane Smith", "birthday": "1990.10.29"}
 ]
 
-today = datetime.today().date()
-upcoming_birthdays = []
-
 def get_upcoming_birthdays(users):
+    today = datetime.today().date()
+    upcoming_birthdays = []
     for user in users:
         birthday = datetime.strptime(user["birthday"], "%Y.%m.%d").date()
         birthday_this_year = birthday.replace(year=today.year)
@@ -21,3 +20,5 @@ def get_upcoming_birthdays(users):
 
 upcoming_birthdays = get_upcoming_birthdays(users)
 print("Список привітань на цьому тижні:", upcoming_birthdays)
+
+

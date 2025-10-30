8bcba7503c319c3a11572a75f738341eff78fd28
Oleksandr Potorocha <pa70rr@gmail.com>
2025-10-30 18:02:03 +0200

    Add files via upload

--- task04.py
+++ task04.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 users = [
     {"name": "John Doe", "birthday": "1985.11.01"},
-    {"name": "Jane Smith", "birthday": "1990.10.29"}
+    {"name": "Jane Smith", "birthday": "1990.11.03"}
 ]
 
 def get_upcoming_birthdays(users):
@@ -14,11 +14,11 @@ def get_upcoming_birthdays(users):
         if birthday_this_year < today:
             birthday_this_year = birthday_this_year.replace(year=today.year + 1)
         if today <= birthday_this_year < today + timedelta(days=7):
+            if birthday_this_year.weekday() >= 5:
+                birthday_this_year += timedelta(days=(7 - birthday_this_year.weekday()))
             upcoming_birthdays.append({"name": user["name"], "congratulation_date": birthday_this_year.strftime("%Y.%m.%d")})
     return upcoming_birthdays
 
 
 upcoming_birthdays = get_upcoming_birthdays(users)
 print("Список привітань на цьому тижні:", upcoming_birthdays)
-
-

5d671938ddb53a53e7ea068c4a50864dd2aa38a9
pa70rr <pa70rr@gmail.com>
2026-02-04 17:29:02 +0200

    new

--- task02.py
+++ task02.py
@@ -1,9 +1,12 @@
-
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    numbers_ticket = set(random.sample(range(min, max), quantity))
-    return sorted(numbers_ticket)
+    if min >= 1 and max <1000 and quantity < (max - min + 1):
+        return sorted(random.sample(range(min, max), quantity))
+    else:
+        return []
+    
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(1, 36, 5)
+
 print("Ваші лотерейні числа:", lottery_numbers)
